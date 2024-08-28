4a0dfe562f4ec2a6076ffc7cac745a269608d0c5
Ongoma Moses <166012251+Ongoma6@users.noreply.github.com>
2024-08-28 11:54:10 +0300

    Create user_input.py
    The user input their details then the machine interpret the answer and give an output to the user 
    Help in make communication imminent and  control how the user communicate 
    help in communities talks and chats

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("what's your name?")
+age = input("How old are you?")
+location=input("Where is your residential sitauted?")
+
+
+print(f"Hello {name},you are {age} years old and your residential is situated in {location}")
